9a20963dc7533acea1bfc70f2ad5c82156207010
Ankichi <59509443+An39nA0219@users.noreply.github.com>
2021-05-11 00:30:23 +0900

    if (num > 0) {     if (num % 3 === 0) {       faceShowFlag || setFaceShowFlag(true);     } else {       faceShowFlag && setFaceShowFlag(false);     }   }

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -8,3 +8,13 @@ const ColorfulMessage = (props) => {
 };
 
 export default ColorfulMessage;
+
+// defaultエクスポート意外にもただのexportというのがある。
+// だたのexportの場合、コンポーネントの関数前にexportを入れる。
+// この場合は、export const ColorfulMessageという感じ
+// 逆にimportの方も書き換えが必要になる。
+// 今まではimport 関数名 form "ファイルの場所"だったけど、
+// ただのexportを設定したときはimport {関数名} from "ファイルの場所"
+// 分割代入をしてあげないといけない。
+// デフォルトエクスポートとの違いは色々あるが、名称を補足してくれたりするので、
+// 通常のエクスポートの方が安全課も（文字間違いに気づいてくれる）
